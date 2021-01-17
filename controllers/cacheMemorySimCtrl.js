f470706ada08cc5972522c221a70231a3e0a7b84
Ivy Tan <tanivy31@gmail.com>
2021-01-17 20:45:46 +0800

    refactored

--- controllers/cacheMemorySimCtrl.js
+++ controllers/cacheMemorySimCtrl.js
@@ -1,16 +1,7 @@
-const cacheAccessTime = 1;
-const memoryAcessTime = 10;
+const { makeCache } = require('../modules/cacheFunc');
 
-function makeCache (d1) {
-    var arr = new Array(d1), i, l;
-    for (i=0, l=d1; i<l; i++) {
-        arr[i] = {
-            MRU: null,
-            cache: new Array()
-        };
-    }
-    return arr;
-}
+const cacheAccessTime = 1;
+const memoryAccessTime = 10;
 
 const cacheMemorySimCtrl = {
     viewSimulationPage: (req, res) => res.render('simulationPage'),
@@ -33,14 +24,31 @@ const cacheMemorySimCtrl = {
             memorySizeUnit = 'block', // block or words
             system = 'decimal',         // hexa or decimal
             systemInput = 'blocks',    // blocks or addresses 
-            firstUpper = 67   
-            firstLower = 0,
+            firstUpper = 1   
+            firstLower = 1,
             firstTime = 10,
-            secondUpper = '',
-            secondLower = '',
-            secondTime = '';
+            secondUpper = 67,
+            secondLower = 0,
+            secondTime = 10;
 
         //converting et al
+        if (cacheSizeUnit === 'words') {
+            cacheSize = cacheSize / blockSize; 
+        }
+
+        if (memorySizeUnit === 'block') {
+            memorySize = memorySize / blockSize;
+        }
+
+        if (systemInput === 'addresses') {
+            if (system === 'decimal') {
+
+            } else if (system === 'hexadecimal') {
+
+            }
+
+        }
+
 
         // Error Trap + add also cacheSize >= setSize
         /**
@@ -60,7 +68,7 @@ const cacheMemorySimCtrl = {
             cacheMiss = 0;
 
         // first loop
-        for (j=0; j<2; j++){
+        for (j=0; j<firstTime; j++){
             for (i=firstLower; i<=firstUpper; i++) {
                 let set = i % numSets;
 
@@ -69,30 +77,36 @@ const cacheMemorySimCtrl = {
 
                 //check if the array has same value
                 let find = row.cache.indexOf(i);
-                
-                console.log(i);
-                console.log(find);
-                console.log('');
 
-                if (find != -1) {
-                    length = find;
-                    cacheHit++;
-                } else {
-                    cacheMiss++;
-                }
+                // if find change the length to index find
+                find !== -1 ? (length = find, cacheHit++) : cacheMiss++;
 
-                if (length < setSize) {
-                    row.cache[length] = i;
-                    row.MRU = length;
-                } else {
-                    // change the value with the MRU
-                    row.cache[row.MRU] = i;
-                }
-
+                // if less than setSize, update value and MRU, else update value only
+                length < setSize ? (row.cache[length] = i, row.MRU = length) : row.cache[row.MRU] = i;
             }
         }
 
         // second loop
+        for (j=0; j<secondTime; j++){
+            for (i=secondLower; i<=secondUpper; i++) {
+                let set = i % numSets;
+
+                let row = cacheMemory[set];
+                let length = row.cache.length;
+
+                //check if the array has same value
+                let find = row.cache.indexOf(i);
+
+                // if find change the length to index find
+                find !== -1 ? (length = find, cacheHit++) : cacheMiss++;
+
+                // if less than setSize, update value and MRU, else update value only
+                length < setSize ? (row.cache[length] = i, row.MRU = length) : row.cache[row.MRU] = i;
+            }
+        }
+
+        //let missPenalty = cacheAccessTime + memoryAccessTime * ? + cacheAccessTime;
+       
 
         console.log(cacheMemory);
         console.log(cacheHit);
