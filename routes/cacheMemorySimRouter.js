0fd372183d1ed3cd8474e71f8ac58ec8d9012679
issertmg <issertmg@gmail.com>
2021-01-21 01:22:04 +0800

    set temporary route for viewing sequential page

--- routes/cacheMemorySimRouter.js
+++ routes/cacheMemorySimRouter.js
@@ -5,4 +5,7 @@ const ctrl = require('../controllers/cacheMemorySimCtrl');
 router.get('/', ctrl.viewHomePage);
 router.post('/TwoLoops', ctrl.postTwoLoops);
 
+// For easy viewing of sequential page (delete if u want)
+router.get('/sequential', (req, res) => res.render('SequentialPage'));
+
 module.exports = router;
