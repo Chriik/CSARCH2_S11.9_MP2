607e2178a948f25b504ed6c0460a80111d23798e
Mikaela Matias <maria_angela_mikaela_matias@dlsu.edu.ph>
2021-01-25 16:11:25 +0800

    drop shadow on sequential page

--- public/javascripts/simulationSequential.js
+++ public/javascripts/simulationSequential.js
@@ -16,16 +16,16 @@ $('#addTaskButton').on("click", function() {
 
     let taskGroup =
         `<div class="task-group">
-              <div class="row"><h3>Task ${nextTaskNumber}</h3></div>
+              <h5>Task ${nextTaskNumber}</h5>
               <div class="row">
                   <div class="form-group col-4">
-                      <label for="lowerRange-${nextTaskNumber}">Lower Range</label>
+                      <label for="lowerRange-${nextTaskNumber}">Lower range</label>
                       <input type="number" class="form-control form-control-sm lower-range" id="lowerRange-${nextTaskNumber}">
                       <small id="lowerError-${nextTaskNumber}" class="text-danger input-error"></small>
                   </div>
 
                   <div class="form-group col-4">
-                      <label for="upperRange-${nextTaskNumber}">Upper Range</label>
+                      <label for="upperRange-${nextTaskNumber}">Upper range</label>
                       <input type="number" class="form-control form-control-sm upper-range" id="upperRange-${nextTaskNumber}">
                       <small id="upperError-${nextTaskNumber}" class="text-danger input-error"></small>
                   </div>
